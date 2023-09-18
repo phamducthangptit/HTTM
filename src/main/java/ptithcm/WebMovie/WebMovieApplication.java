98226eb2e72da7d05a84e0b295bc0df777bd9674
phamducthangptit <pthang2506@gmail.com>
2023-09-18 15:49:54 +0700

    first

---
+++ src/main/java/ptithcm/WebMovie/WebMovieApplication.java
@@ -0,0 +1,13 @@
+package ptithcm.WebMovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebMovieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ptithcm/WebMovie/WebMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package ptithcm.WebMovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
