3fc0ea16e15f909640390b97b2596294add2380e
phamducthangptit <pthang2506@gmail.com>
2023-09-29 13:50:53 +0700

    update collection

--- src/main/java/ptithcm/WebMovie/Service/MovieCollectionService.java
+++ src/main/java/ptithcm/WebMovie/Service/MovieCollectionService.java
@@ -1,7 +1,12 @@
 package ptithcm.WebMovie.Service;
 
+import java.util.List;
+import java.util.Map;
+
 public interface MovieCollectionService {
     int addMovieToCollection(int userId, int movieId);
 
     int removeMovieToCollection(int userId, int movieId);
+
+    List<Map<String, ?>> findMyCollection(int userId);
 }
